e2518b60a0a252225aaf6f61f21d9a030ef27429
Matthias <matthias.duffour@gmail.com>
2023-10-31 17:33:08 +0100

    Desktop final version

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 var img = "-img-";
 var desc = "-desc-";
 
-disableScroll();
 showChoice();
 
 
@@ -23,15 +22,3 @@ function showChoice() {
         }
     }
 }
-
-function disableScroll() {
-    // Get the current page scroll position
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
-        // if any scroll is attempted, set this to the previous value
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-    
